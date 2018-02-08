3c79d39aaeaa3c482837a24f5244a88d39c36de8
Taylor Liss <tliss@camsys.com>
2018-02-08 15:55:26 -0500

    Add shark code

---
+++ ch02/starfishcollectorbeta/core/src/com/tayloraliss/starfishcollectorbeta/Shark.java
@@ -0,0 +1,8 @@
+package com.tayloraliss.starfishcollectorbeta;
+
+public class Shark extends ActorBeta {
+
+    public Shark() {
+        super();
+    }
+}
--- ch02/starfishcollectorbeta/core/src/com/tayloraliss/starfishcollectorbeta/StarfishCollectorBeta.java
+++ ch02/starfishcollectorbeta/core/src/com/tayloraliss/starfishcollectorbeta/StarfishCollectorBeta.java
@@ -11,11 +11,14 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 public class StarfishCollectorBeta extends GameBeta 
 {
     private Turtle turtle;
+    private Shark shark;
     private ActorBeta starfish;
     private ActorBeta ocean;
     private ActorBeta winMessage;
+    private ActorBeta gameOverMessage;
     
     private boolean win;
+    private boolean gameOver;
     
     public void initialize() 
     {        
@@ -32,14 +35,26 @@ public class StarfishCollectorBeta extends GameBeta
         turtle.setTexture( new Texture(Gdx.files.internal("turtle-1.png")) );
         turtle.setPosition( 20,20 );
         mainStage.addActor( turtle );
+
+        shark = new Shark();
+        shark.setTexture( new Texture(Gdx.files.internal("sharky.png")) );
+        shark.setPosition(Gdx.graphics.getWidth() / 2 - shark.getWidth() / 2,  Gdx.graphics.getHeight() / 2 - shark.getHeight() / 2);
+        mainStage.addActor( shark );
         
         winMessage = new ActorBeta();
         winMessage.setTexture( new Texture(Gdx.files.internal("you-win.png")) );
         winMessage.setPosition( 180,180 );
         winMessage.setVisible( false );
         mainStage.addActor( winMessage );
+
+        gameOverMessage = new ActorBeta();
+        gameOverMessage.setTexture( new Texture(Gdx.files.internal("game-over.png")) );
+        gameOverMessage.setPosition(180, 180);
+        gameOverMessage.setVisible( false );
+        mainStage.addActor( gameOverMessage );
         
         win = false;
+        gameOver = false;
     }
 
     public void update(float dt)
@@ -49,6 +64,11 @@ public class StarfishCollectorBeta extends GameBeta
         {
             starfish.remove();
             winMessage.setVisible(true);
+        }
+
+        if (turtle.overlaps(shark)){
+            shark.remove();
+            gameOverMessage.setVisible(true);
         }
     }
 }
