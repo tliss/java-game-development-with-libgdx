b03be3daeea24b8ec44cc0ca0ef8fc2c4ccc8b52
Taylor A. Liss <tayloraliss@gmail.com>
2018-02-19 02:28:05 -0500

    Add setAnimation method

--- ch03/mystarfishcollector/core/src/com/tayloraliss/mystarfishcollector/BaseActor.java
+++ ch03/mystarfishcollector/core/src/com/tayloraliss/mystarfishcollector/BaseActor.java
@@ -1,10 +1,16 @@
 package com.tayloraliss.mystarfishcollector;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class BaseActor extends Actor
 {
+    private Animation<TextureRegion> animation;
+    private float elapsedTime;
+    private boolean animationPaused;
+
     public BaseActor(float x, float y, Stage s)
     {
         // call constructor from Actor class
@@ -12,5 +18,17 @@ public class BaseActor extends Actor
         // perform additional initialization tasks
         setPosition(x,y);
         s.addActor(this);
+        animation=null;
+        elapsedTime=0;
+        animationPaused=false;
+    }
+
+    public void setAnimation(Animation<TextureRegion> anim){
+        animation = anim;
+        TextureRegion tr = animation.getKeyFrame(0);
+        float w = tr.getRegionWidth();
+        float h = tr.getRegionHeight();
+        setSize(w, h);
+        setOrigin(w/2, h/2);
     }
 }
