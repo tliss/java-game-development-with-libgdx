55d0d474ce6669f3993e2d6b3347f784615e5d80
Taylor A. Liss <tayloraliss@gmail.com>
2018-02-19 23:56:42 -0500

    Add missising files for animations

---
+++ ch03/mystarfishcollector/core/src/com/tayloraliss/mystarfishcollector/StarfishCollector.java
@@ -0,0 +1,20 @@
+package com.tayloraliss.mystarfishcollector;
+
+public class StarfishCollector extends GameBeta {
+    private Turtle turtle;
+    private Starfish starfish;
+    private BaseActor ocean;
+
+    public void initialize() {
+        ocean = new BaseActor(0, 0, mainStage);
+        ocean.loadTexture("water.jpg");
+        ocean.setSize(800, 600);
+
+        starfish = new Starfish(380, 380, mainStage);
+        turtle = new Turtle(20, 20, mainStage);
+    }
+
+    public void update(float dt) {
+        //TODO: Add code later
+    }
+}
